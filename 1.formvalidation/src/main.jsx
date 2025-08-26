85d2e7f142ad0d7599482654dd5efe47f0e92b5e
Samar Ali <106392180+Samarali910@users.noreply.github.com>
2025-08-26 08:53:30 +0530

    remove unnecassery fields

--- 1.formvalidation/src/main.jsx
+++ 1.formvalidation/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Formvalidation from './formvalidation.jsx'
+ 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
 
-    {/* <Formvalidation/> */}
+    
     
   </StrictMode>,
 )
